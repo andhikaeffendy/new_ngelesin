42b3288ee1a59db499e032647424db8d6c290640
Daniel <kingsantha16af@gmail.com>
2020-03-16 12:06:35 +0700

    Get Kota Selesai

--- lib/list_kota.dart
+++ lib/list_kota.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:new_ngelesin/api_response_model/list_kota_response.dart';
+import 'global_variable/temp_var.dart' as globTemp;
 
 class ListKota extends StatefulWidget {
   @override
@@ -20,20 +23,47 @@ class _ListKotaState extends State<ListKota> {
 
   @override
   Widget build(BuildContext context) {
+    print("id provinsi : " + globTemp.provId.toString());
     return Scaffold(
       body: Container(
-        child: ListView.builder(
-            itemCount: mKota.length,
-            itemBuilder: (BuildContext context, int index) {
-              final provinsi = mKota[index].toString();
-              return Card(
-                child: ListTile(
-                  title: Text(provinsi),
-                  onTap: () {Navigator.pop(context,mKota[index].toString());},
-                ),
-              );
-            }),
+        child: FutureBuilder(
+          future: kotaRequest(),
+          builder: (context, snapshot){
+            if(snapshot.data == null){
+              return Container();
+            }else{
+              ListKotaResponse listKotaResponse = snapshot.data;
+              List listKota = listKotaResponse.data;
+              return ListView.builder(
+                  itemCount: listKota.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final Datum kota = listKota[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text(kota.kota),
+                        onTap: () {
+                          globTemp.kotaId = kota.id;
+                          Navigator.pop(context,kota.kota.toString());},
+                      ),
+                    );
+                  });
+            }
+          },
+        )
       ),
     );
+  }
+
+  Future<ListKotaResponse> kotaRequest() async {
+    String url = "http://apingelesin.com/app/api/web/index.php?r=v1/home/kota&provinsi_id=" + globTemp.provId.toString();
+    Dio dio = new Dio();
+    Response response;
+
+    response = await dio.get(url);
+    print(response.toString());
+
+    ListKotaResponse kotaResponse = listKotaResponseFromJson(response.toString());
+
+    return kotaResponse;
   }
 }
