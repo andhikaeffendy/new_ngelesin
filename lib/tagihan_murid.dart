de3dc8e3a840fa7afbc54a218ff4d27bd3b7db32
andhikaeffendy14 <andhikaeffendy14@gmail.com>
2020-04-04 11:17:59 +0700

    tagihan

--- lib/tagihan_murid.dart
+++ lib/tagihan_murid.dart
@@ -10,48 +10,82 @@ class TagihanMurid extends StatefulWidget {
 }
 
 class _TagihanMuridState extends State<TagihanMurid> {
+
+  final List<String> kode = <String> ['M14296','2]JFK98','MPOD3','EIRKD4'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tagihan'),
       ),
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Icon(
-                  Icons.unarchive,
-                  size: 80.0,
+        body: ListView.builder(
+            itemCount: kode.length,
+            itemBuilder: (BuildContext context, int index){
+              return GestureDetector(
+                onTap: _showAlertDialog,
+                child: Container(
+                  child: Card(
+                    elevation: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text('5LD237', style: TextStyle(fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text('21-Apr-2020'),
+                            )
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              width: 150.0,
+                              padding: const EdgeInsets.only(bottom: 4.0),
+                              child: Text('Mengaji (Lain-Lain)', style: TextStyle(fontWeight: FontWeight.bold),),
+                            ),
+                            Container(
+                              width: 150.0,
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text('Tedi Guru'),
+                            )
+                          ],
+                        ),Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Container(
+                              width: 120.0,
+                              child: Text(
+                                'CASH'
+                                ,style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              width: 120.0,
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                'Rp. 200.000', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
-                ),
-                FutureBuilder(
-                  future: getTagihanRequest(),
-                  builder: (context, snapshot){
-                    if(snapshot.data!=null){
-                      TagihanResponse tagihanData = snapshot.data;
-                      return Text(
-                        tagihanData.message,
-                        style: new TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: Colors.blueGrey,
-                            fontSize: 16.0),
-                        textAlign: TextAlign.center,
-                      );
-                    }else{
-                      return Container();
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        )
+              );
+            })
     );
   }
 
@@ -81,5 +115,160 @@ class _TagihanMuridState extends State<TagihanMurid> {
 
     return tagihanResponse;
 
+  }
+
+  void _showAlertDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Container(
+              width: 330.0,
+              height: 350.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'No Order',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            child: Text('YOPESO'),
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'Pembayaran',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            child: Text('Saldo'),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'Mapel / Bidang',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            child: Text('Matermatika SD'),
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'Jumlah Murid',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            child: Text('1'),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Container(
+                    child: Text(
+                      'Tanggal Dan Waktu',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    child: Text('30-Mar-2020 08:00:00 - 10:00:00'),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Container(
+                    child: Text(
+                      'Nama Guru',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    child: Text('Rizky Akbar'),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Container(
+                    child: Text(
+                      'Lokasi & Jam: ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    child: Text('rumah saya,'),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Container(
+                    child: Text(
+                      'Biaya:',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Rp. 75.000',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),ButtonTheme(
+                    minWidth: 300.0,
+                    child: RaisedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text('TUTUP', style: TextStyle(color: Colors.white),),
+                      color: Colors.blue,),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
